d1212c3418550bbee2e863d3696c2badde61d880
ec50n9 <shelloworld@qq.com>
2023-10-01 21:03:28 +0800

    增加单词翻译功能

--- src/api/methods/word.ts
+++ src/api/methods/word.ts
@@ -7,3 +7,23 @@ export const queryWord = (word: string) =>
 
 export const recordWord = (words: string[]) =>
   commonAlova.Post("/record-word", { words });
+
+export const translate = (
+  word: string | string[],
+  from?: string,
+  to?: string
+) => {
+  const query = typeof word === "string" ? word : word.join("\n");
+  return commonAlova.Post<{ word: string; translation: string }[]>(
+    "/translate",
+    { query, from, to },
+    {
+      transformData(data, headers) {
+        console.log(data.data.query);
+        const query = data.data.query.split("\n").map((item) => item.trim());
+        const result = data.data.translation[0].split("\n").map((item) => item.trim());
+        return query.map((q, i) => ({ word: q, translation: result[i] }));
+      },
+    }
+  );
+};
--- src/views/record-word/index.vue
+++ src/views/record-word/index.vue
@@ -1,23 +1,45 @@
 <script lang="ts" setup>
 import CommonHeader from "@/components/common-header.vue";
-import { NInput, NButton, useMessage } from "naive-ui";
+import {
+  NInput,
+  NButton,
+  NDrawer,
+  NDrawerContent,
+  NIcon,
+  useMessage,
+} from "naive-ui";
+import { ArrowForwardRound } from "@vicons/material";
 import { ref } from "vue";
 import { invalidateCache, useRequest } from "alova";
-import { recordWord, listMyWords } from "@/api/methods/word";
+import { recordWord, listMyWords, translate } from "@/api/methods/word";
 
 const message = useMessage();
 
 const inputValue = ref("");
 
+// 上传单词接口
 const recordWordReq = useRequest(recordWord, { immediate: false });
 recordWordReq.onError((err) => {
   message.error(err.error.message);
 });
 recordWordReq.onSuccess(() => {
-  message.success("添加成功");
   inputValue.value = "";
   invalidateCache(listMyWords());
+  message.success("添加成功");
+  drawerVisible.value = false;
 });
+
+// 翻译接口
+const translateReq = useRequest(translate, { immediate: false });
+translateReq.onError((err) => {
+  message.error(err.error.message);
+});
+translateReq.onSuccess((_res) => {
+  drawerVisible.value = true;
+});
+
+// 翻译结果查看
+const drawerVisible = ref(false);
 
 /**
  * 统计字符串中出现频率最高的非字母字符
@@ -46,7 +68,8 @@ const getMostFrequentNonLetter = (str: string) => {
   return maxChar;
 };
 
-const handleSubmit = () => {
+// 提交事件
+const handleTranslate = () => {
   if (inputValue.value.trim() === "") return message.warning("请输入单词");
 
   const mostFrequentNonLetter = getMostFrequentNonLetter(inputValue.value);
@@ -55,9 +78,10 @@ const handleSubmit = () => {
         .split(mostFrequentNonLetter)
         .filter((word) => word.trim() !== "")
     : [inputValue.value];
-  recordWordReq.send(words);
+  translateReq.send(words);
 };
 
+// 通过上传文件导入
 const handleUpload = () => {
   const input = document.createElement("input");
   input.type = "file";
@@ -74,6 +98,11 @@ const handleUpload = () => {
     reader.readAsText(file);
   };
   input.click();
+};
+
+// 上传单词记录
+const handleUploadRecord = () => {
+  recordWordReq.send(translateReq.data.value.map((item) => item.word));
 };
 </script>
 
@@ -95,11 +124,43 @@ const handleUpload = () => {
         <NButton @click="handleUpload">选择文件</NButton>
         <NButton
           type="primary"
-          :loading="recordWordReq.loading.value"
-          @click="handleSubmit"
+          :loading="translateReq.loading.value"
+          @click="handleTranslate"
           >提交</NButton
         >
       </div>
     </div>
+
+    <n-drawer v-model:show="drawerVisible" placement="bottom" height="90%">
+      <n-drawer-content header-style="display:block;">
+        <template #header>
+          <div class="flex justify-between items-center">
+            <span>翻译结果</span>
+            <n-button
+              class="self-end"
+              size="medium"
+              type="primary"
+              :loading="recordWordReq.loading.value"
+              @click="handleUploadRecord"
+            >
+              确认添加
+            </n-button>
+          </div>
+        </template>
+
+        <ul class="flex flex-wrap gap-3">
+          <li
+            v-for="item in translateReq.data.value"
+            class="px-3 py-2 flex justify-between items-center gap-3 b rd-3"
+          >
+            <span>{{ item.word }}</span>
+            <n-icon class="text-lg c-gray-3">
+              <arrow-forward-round />
+            </n-icon>
+            <span>{{ item.translation }}</span>
+          </li>
+        </ul>
+      </n-drawer-content>
+    </n-drawer>
   </div>
 </template>
